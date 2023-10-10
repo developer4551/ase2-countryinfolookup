a108f1f6865ad383ff85cb6c407ba5da000559a6
developer4551 <prof.inbox.11@gmail.com>
2023-10-10 14:56:37 +0100

    Issue#4 - UI- add React hook (useState) to persist user selected option, added tests, javascript functions on search click/enter

--- src/View/HomePage.js
+++ src/View/HomePage.js
@@ -1,56 +1,83 @@
 import '../App.css';
 import '../css/HomePage.css';
+import React, { useState } from 'react';
 
 
 const searchDropdownOptions = {
-  'Please select appropriate search option': [
+    'Select search option': [
     'null',
     'Search by country name, country code, country capital.',
   ],
   Capital: ['capital', 'Enter country\'s capital.'], 
-  Code: ['alpha', 'Enter country code. '],
+  Code: ['code', 'Enter country code. '],
   Name: ['name', 'Enter country\'s full name.']
 };
 
 function HomePage() {
 
+  const handleSearchOptionSelected = (event) => {
+    setInputData({...inputData,[event.target.name]: event.target.value,});
+
+    if (`${event.target.value}` in searchDropdownOptions) {
+      setInputData({ ...inputData, [event.target.name]: `${searchDropdownOptions[event.target.value][0]}`,});
+      console.log(searchDropdownOptions[event.target.value][0]);
+      setDefaultText(searchDropdownOptions[event.target.value][1]);
+    }
+  };
+
+  const [inputData, setInputData] = useState({inputText: '', searchOption: '',});
+  const [defaultText, setDefaultText] = useState('Enter country name, capital or code.');
+
+  const handleSearchButtonClick = (event) => {
+    if (event.keyCode === 13) {
+      // Call external REST api here
+      console.log("Search button clicked");
+    }
+  };
   return (
     <div className='app-container'>
       <div className='search-container'>
         <div className='inputs-container'>
+
           <div className='form-align'>
-            <label for='selectOpt' className='search-dropdown-label'>Choose search option</label>
+            <label htmlFor='selectOpt' className='search-dropdown-label'>Choose search option</label>
             <select
               name='searchOption'
               id="selectOpt"
               className='search-dropdown'
               data-testid='select'
+              onChange={handleSearchOptionSelected}
             >
               {Object.keys(searchDropdownOptions).map((item) => (
                 <option
                   value={
-                    item !== 'Please select a search option' ? item : 'null'
+                  item !== 'Please select a search option' ? item : 'null'
                   }
                   key={item}
                   data-testid="dropdown-options"
+                  className='search-dropdown-option'
                 >
                   {item}
                 </option>
               ))}
             </select>
           </div>
+
           <div className='search-textbox-outer-container'>
             <div className='search-textbox-inner-container'>
-              <label for='default-search' className='search-dropdown-label'>Please provide input </label>
+              <label htmlFor='default-search' className='search-dropdown-label'>Please provide input </label>
 
               <input
                 type='text'
                 id='default-search'
                 className='search-text-box'
-                required
                 name='inputText'
                 data-testid='searchTextBox'
+                placeholder={defaultText}
+                onKeyDown={(e) => handleSearchButtonClick(e)}
+                required
               />
+
               <button
                 type='submit'
                 className='search-text-box-button'
--- src/tests/ui/App.test.js
+++ src/tests/ui/App.test.js
@@ -2,10 +2,11 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../../App';
 import { BrowserRouter } from 'react-router-dom';
 
+describe("checking_UI_Components", () => {
 test('renders Welcome to Country Information Lookup text', () => {
   render(<BrowserRouter>
     <App />
-  </BrowserRouter> );
+  </BrowserRouter>);
   const linkElement = screen.getByText(/Welcome to Country Information Lookup/i);
   expect(linkElement).toBeInTheDocument();
 });
@@ -13,20 +14,23 @@ test('renders Welcome to Country Information Lookup text', () => {
 test('renders choose search option textbox label', () => {
   render(<BrowserRouter>
     <App />
-  </BrowserRouter> );
+  </BrowserRouter>);
   const linkElement = screen.getByText(/Choose search option/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders search options dropdown', () => {
-  const {getByTestId, getAllByTestId } = render(<BrowserRouter>
+  //Arrange
+  const { getByTestId, getAllByTestId } = render(<BrowserRouter>
     <App />
-  </BrowserRouter> );
-  fireEvent.change(getByTestId('select'), {target: {value : 'Capital'}});
+  </BrowserRouter>);
+
+  //Act
+  fireEvent.change(getByTestId('select'), { target: { value: 'Capital' } });
   let searchOptions = getAllByTestId('dropdown-options');
-  
-  console.log(searchOptions[0].innerHTML);
-  expect(searchOptions[0].innerHTML).toEqual('Please select appropriate search option');
+
+  //Assert
+  expect(searchOptions[0].innerHTML).toEqual('Select search option');
   expect(searchOptions[1].innerHTML).toEqual('Capital');
   expect(searchOptions[2].innerHTML).toEqual('Code');
   expect(searchOptions[3].innerHTML).toEqual('Name');
@@ -40,10 +44,38 @@ test('renders search options dropdown', () => {
 test('renders search textbox and search button', () => {
   render(<BrowserRouter>
     <App />
-  </BrowserRouter> );
+  </BrowserRouter>);
   const textBoxElement = screen.getByTestId(/searchTextBox/);
-  expect(textBoxElement).toBeInTheDocument();
   const buttonElement = screen.getByTestId(/searchBtn/);
+  expect(textBoxElement).toBeInTheDocument();
   expect(buttonElement).toBeInTheDocument();
+});
 
+test('search option selected correctly', () => {
+  const { getByTestId, getAllByTestId } = render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
+
+  fireEvent.change(getByTestId('select'), { target: { value: 'Name' } });
+  let searchOptions = getAllByTestId('dropdown-options');
+
+  expect(searchOptions[0].selected).toBeFalsy();
+  expect(searchOptions[1].selected).toBeFalsy();
+  expect(searchOptions[2].selected).toBeFalsy();
+  expect(searchOptions[3].selected).toBeTruthy();
+});
+
+test('placeholder text displayed in search textbox', () => {
+  const { getByTestId, getAllByTestId } = render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
+
+  fireEvent.change(getByTestId('select'), { target: { value: 'Name' } });
+  let placeholderElement = screen.queryByPlaceholderText(/Enter country\'s full name./i);
+  expect(placeholderElement).toBeInTheDocument();
+});
+});
+
+
+describe("test_SearchByName_ExternalApi", () => {
 });
